70152cecfc233d603eca2a40bb818ac19df070eb
AnniekStok <anniek.stokkermans@gmail.com>
2026-09-15 16:28:34 +0200

    keep the selected track active after clearing the selection, it is harmless, and ensures the next painting color is consistent with what is shown

--- src/motile_tracker/data_views/views_coordinator/tracks_viewer.py
+++ src/motile_tracker/data_views/views_coordinator/tracks_viewer.py
@@ -428,8 +428,6 @@ class TracksViewer:
 
         if len(self.selected_nodes) > 0:
             self.selected_track = self.tracks.get_track_id(self.selected_nodes[-1])
-        else:
-            self.selected_track = None
 
         self.set_track_id_color(self.selected_track)
         self.update_track_id.emit()
