acce499e668687b4dfe7c8a2d1ffb94eebd9ffe3
AnniekStok <anniek.stokkermans@gmail.com>
2026-09-24 18:09:31 +0200

    fix label in welcome widget test

--- tests/application_menus/test_welcome_widget.py
+++ tests/application_menus/test_welcome_widget.py
@@ -13,11 +13,11 @@ def test_welcome_widget_initializes(qtbot):
     assert widget is not None
     # Should have a layout
     assert widget.layout() is not None
-    # Should contain a QLabel with 'Motile Tracker'
+    # Should contain a QLabel with 'Napari Track Edit'
     found_label = False
     for i in range(widget.layout().count()):
         item = widget.layout().itemAt(i).widget()
-        if isinstance(item, QLabel) and "Motile Tracker" in item.text():
+        if isinstance(item, QLabel) and "Napari Track Edit" in item.text():
             found_label = True
     assert found_label
     # Should contain at least one QTextBrowser
