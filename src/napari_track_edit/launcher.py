29cb9b575ee6e3d9da1f0992cc8264a7a57ae81f
AnniekStok <anniek.stokkermans@gmail.com>
2026-09-24 17:31:26 +0200

    update name in dock widget launcher

--- src/napari_track_edit/launcher.py
+++ src/napari_track_edit/launcher.py
@@ -2,7 +2,7 @@ import napari
 
 # Auto-load your plugin
 viewer = napari.Viewer()
-viewer.window.add_plugin_dock_widget("motile-tracker")
+viewer.window.add_plugin_dock_widget("napari-track-edit")
 
 # Start Napari event loop
 napari.run()
